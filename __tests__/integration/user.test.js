e5e022435aae2913afa44b6737eb2e2282586ad2
Edson <edsaraujocorral12@gmail.com>
2020-06-19 15:00:47 -0300

    Testando a Criptografia de Senha

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,5 +1,7 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
+import User from '../../src/app/models/User';
 
 import truncate from '../util/truncate';
 
@@ -17,6 +19,18 @@ describe('User', () => {
         password_hash: '1231123',
       });
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should encrypt user password when new user created', async () => {
+    const user = await User.create({
+      name: 'Edson',
+      email: 'edsaraujocorral12@gmail.com',
+      password: '1231123',
+    });
+
+    const compareHash = await bcrypt.compare('1231123', user.password_hash);
+
+    expect(compareHash).toBe(true);
   });
 
   it('shoud not be able to register with duplicated email', async () => {
